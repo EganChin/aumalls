635668b6cea3410856fc82090fd08af0bd6b726b
JackCaiXing <836310194@qq.com>
2019-07-12 23:03:48 +0800

    批量下架商品完成-CCX

--- aumalls-goods/src/main/java/com/mall/goods/dao/GoodsManagerDao.java
+++ aumalls-goods/src/main/java/com/mall/goods/dao/GoodsManagerDao.java
@@ -12,6 +12,26 @@ import java.util.List;
  */
 @Repository
 public interface GoodsManagerDao extends BaseMapper<Goods> {
+    /**
+     * 审批商品
+     * 返回商品状态state为0的所有商品
+     * @return
+     */
     public List<Goods> getGoodsByState0();
 
+    /**
+     * 管理员同意商品上架
+     * 更新商品状态state为1
+     * @param goodId
+     * @return
+     */
+    public int updateGoodsState(Integer goodId);
+
+    /**
+     * 管理员下架商品
+     * 更新商品状态state为0
+     * @param ids
+     * @return
+     */
+    public int updateGoodsStateTo0(List ids);
 }
