ec4c0f8557a4baacef4f1d97baf1bf8c03d716b0
JackCaiXing <836310194@qq.com>
2019-07-14 11:43:03 +0800

    2019/7/14 11:41-CCX

---
+++ aumalls-common/src/main/java/com/mall/common/form/goods/ManagerGoodsForm.java
@@ -0,0 +1,9 @@
+package com.mall.common.form.goods;
+
+import com.mall.common.domain.Goods;
+import com.mall.common.utils.Query;
+
+import java.io.Serializable;
+
+public class ManagerGoodsForm extends Query implements Serializable {
+}
