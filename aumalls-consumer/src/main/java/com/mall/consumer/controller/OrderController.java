1fd41119bb5d34aaaaa151a7048642b2ec4a195a
WWW1065324668 <1065324668@qq.com>
2019-07-14 13:58:46 +0800

    Commit

--- aumalls-consumer/src/main/java/com/mall/consumer/controller/OrderController.java
+++ aumalls-consumer/src/main/java/com/mall/consumer/controller/OrderController.java
@@ -1,12 +1,12 @@
 package com.mall.consumer.controller;
 
 import com.mall.common.form.order.QueryOrdersForm;
+import com.mall.common.service.OrderService;
 import com.mall.common.utils.R;
 import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.mall.order.service.OrderServiceImpl;
 
 /**
  * @author 10653
@@ -18,11 +18,11 @@ import com.mall.order.service.OrderServiceImpl;
 public class OrderController extends  BaseController{
 
     @Reference
-    private OrderServiceImpl orderServiceImpl;
+    private OrderService orderService;
 
     @RequestMapping(value = "ordersPage", method = RequestMethod.GET)
     public R getPage(QueryOrdersForm form){
         int userId=getUser().getUserId();
-        return R.ok().put("page", orderServiceImpl.getOrdersByUserId(userId,form));
+        return R.ok().put("page", orderService.getOrdersByUserId(userId,form));
     }
 }
